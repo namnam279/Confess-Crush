2abe96f564b1f376718ff7c052a9a1c837d706a9
namnam279 <85667096+namnam279@users.noreply.github.com>
2022-01-10 00:57:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    introTitle: 'mèo phương anh của a',
+    introDesc: `Trái đất vốn có 7 tỷ người nhưng sao a 
+              Lại Có thể gặp dc em^^
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'sau này cùng a,khoảng tgian còn lại nha..<33',
+    btnNo: 'Không pé ơiiii :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'anh biết mà 🥰. Yêu em siuuuuuu nhiều 😘😘',
+    messDesc: 'tương lai say này,để a đc ở bên che chở cho em nhé.',
+    btnAccept: 'Okiiiii nunnnnnnn <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //https://www.facebook.com/profile.php?id=100069740943400.
 }
